07752e0926a99f6a187973e00524627bf7a6e278
AndersonDuranH <anderson_duran@ucne.edu.do>
2026-04-05 11:56:12 -0400

    Add files via upload

---
+++ NodoArbol.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Jerarquia
+{
+    public class Empleado
+    {
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+        public TablaHashContactos Directorio { get; set; }
+        public Empleado[] Subordinados { get; set; }
+        public int CantSubordinados { get; set; }
+        public Empleado(string cod, string nom, string ape, int ed)
+        {
+            Codigo = cod;
+            Nombre = nom;
+            Apellido = ape;
+            Edad = ed;
+
+            Directorio = new TablaHashContactos(50);
+
+            Subordinados = new Empleado[50]; 
+            CantSubordinados = 0;
+        }
+
+        public int ObtenerEsfuerzo()
+        {
+            return Directorio.NumElementos;
+        }
+
+        public override string ToString()
+        {
+            return $"[{Codigo}] {Nombre} {Apellido} (Esfuerzo: {ObtenerEsfuerzo()})";
+        }
+    }
+}
